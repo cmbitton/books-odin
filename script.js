88abbc622ba99f9839b09ce760c9bca6644e38a0
Curtis <cmbitton@gmail.com>
2023-01-03 15:02:46 -0500

    Added functions to add and display books
    Added functions to add a book to library and display books in
    library in a grid format. Added preliminary CSS for basic styling

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
+let myLibrary = [];
+
 function Books(title, author, pageNumber, hasRead) {
-  (this.title = title),
-    (this.author = author),
-    (this.pageNumber = pageNumber),
-    (this.hasRead = hasRead),
-    (this.info = () =>
-      `${this.title}, by ${this.author}, ${pageNumber} pages, ${hasRead}`);
+  this.title = title,
+    this.author = author,
+    this.pageNumber = pageNumber,
+    this.hasRead = hasRead
 }
 
-const book = new Books('the great gatsby', 'f-scott fitz', 200, 'has read')
-console.log(book.info())
+Books.prototype.info = function () {
+  return `${this.title}, by ${this.author}, ${this.pageNumber} pages, ${this.hasRead}`;
+}
+
+function addBookToLibrary(title, author, pageNumber, hasRead) {
+  const book = new Books(title, author, pageNumber, hasRead);
+  myLibrary.push(book);
+}
+
+function displayBooks(){
+  const mainContent = document.querySelector('main');
+  for(const book of myLibrary){
+    const contentBox = document.createElement('div');
+    const bookTitle =  document.createElement('h2');
+    const bookAuthor = document.createElement('p');
+    const pageNumber = document.createElement('p');
+
+    contentBox.classList.add('book-container');
+    bookTitle.classList.add('book-title');
+    bookAuthor.classList.add('book-author');
+    pageNumber.classList.add('book-page-number');
+
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = book.author;
+    pageNumber.textContent = book.pageNumber;
+
+    contentBox.append(bookTitle);
+    contentBox.append(bookAuthor);
+    contentBox.append(pageNumber);
+    mainContent.append(contentBox)
+  }
+}
+
+addBookToLibrary('Great Gatsby', 'fscott', '200', false);
+addBookToLibrary('Diary of a wimpy Kid', 'Jeff Kinney', '150', true);
+displayBooks();
